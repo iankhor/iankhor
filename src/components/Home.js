3e8e8501f9e0e54a315a202923a280814a878e15
iankhor <iankhorbc.thecoder@gmail.com>
2017-02-26 21:54:34 +1100

    updated home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,42 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import '../css/style.css'
 
 class Home extends Component {
-
-  state = {
-    shouldShowBox: false
-  }
-
-  toggleBox = () => {
-    this.setState({
-      shouldShowBox: !this.state.shouldShowBox
-    })
-  }
-
-  componentDidMount(){
-    this.toggleBox()
-    setTimeout( this.haha, 4000)
-  }
-
-  haha = () => {
-    console.log('haha')
-
-  }
 
   render() {
     return (
       <div className="home-body">
         <div className="row name around-xs">
-          <div className="col-xs-12 col-md-6">
-              <div className="box firstname border">I<span className="dev-red">A</span>N</div>
+        <ReactCSSTransitionGroup
+            transitionName="example"
+            transitionAppear={true}
+            transitionAppearTimeout={2000}
+            transitionEnter={false}
+            transitionLeave={false}
+            className="border">
+          <div className="col-xs-12 col-md-4">
+              <div className="box firstname">I<span className="dev-red">A</span>N</div>
           </div>
-          <div className="col-xs-12 col-md-6">
+      </ReactCSSTransitionGroup>
+          
+          <div className="col-xs-12 col-md-4">
               <div className="box lastname ">KHOR</div>
           </div>
         </div>
 
         <div className="row around-xs">
           <div className="col-xs-12">
+
+            
+
               <div className="box in-transit">... <span className="dev-red">i</span>n tran<span className="dev-red">s</span>it ...</div>
           </div>
         </div>
       
       </div>
+
+      
 
         
        
